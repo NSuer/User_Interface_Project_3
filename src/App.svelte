3fb7c7724e156a251d04e32d6ef17013f6d57759
Owen Richards <owencrichards@gmail.com>
2024-11-10 12:36:34 -0500

    Added Products "page"

--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,6 @@
 <script>
   let x = 0, count1 = 1, count2 = 1, count3 = 1, count4 = 1;
+  let productsCheck = true;
 
   function addtocart() {
     x += 1;
@@ -20,7 +21,6 @@
   }
 
   function incrementDown(val) {
-    
     if (val == "count1") {
       if (count1 > 0) {
         count1 -= 1;
@@ -39,6 +39,14 @@
       }
     }
   }
+
+  function showProducts() {
+    productsCheck = false;
+  }
+  function showHome() {
+    productsCheck = true;
+  }
+
 </script>
 
 <!-- Improved UI Changes  -->
@@ -52,13 +60,14 @@
     <input placeholder="Search for...">
   </section>
   <nav>
-    <button>Account</button>
+    <button on:click={showHome}>Home</button> <!-- Added home button to navigation -->
+    <button on:click={showProducts}>Products</button>
     <!--<button>Checkout</button>-->
     <button>Custom Blinkees</button>
-    <button>Products</button>
+    <button>Account</button>
     <button>Contact</button>
     <button>About</button>
-    <button>Menu Item</button>
+    <button>Menu Item</button> <!-- this button on the action page leads to coins?!? -->
     <section>
       <img src="src/assets/1413908.png" width=18>
       <label for="shopping-cart">{x}</label>
@@ -66,60 +75,176 @@
   </nav>
 </header>
 <main>
-  <div class="menu-row">
-    <div class="menu-item">
-      <img src="src/assets/Light-Up-Pirate-Rapier-Expandable-Sword-Main-350x350.webp" width=300>
-      <h4 style="line-height: 0px;">As low as $4.89 Bulk</h4>
-      <section style="position: relative; display: flex; flex-direction: row; gap: 10px; margin: auto; width: 23%; align-items: center; line-height: 0px;">
-        <button class="increments" on:click={() => incrementDown("count1")} style="cursor: pointer; padding: 2px 5px; border-radius: 20px; border: 1px solid black;">-</button>
-        <p>{count1}</p>
-        <button class="increments" on:click={() => incrementUp("count1")} style="cursor: pointer; padding: 3px 2px; border-radius: 20px; border: 1px solid black;">+</button>
-      </section> 
-      <h2>Light Up Pirate Rapier Expandable Sword</h2>
-      <h4 style="line-height: 0px;">$7.69</h4>
-      <button style="margin-bottom: 10px; cursor: pointer;">Add to Cart</button><br>
-      <button style="cursor: pointer;">Buy in Bulk</button>
+  {#if productsCheck}
+    <div class="menu-row">
+      <div class="menu-item">
+        <img src="src/assets/Light-Up-Pirate-Rapier-Expandable-Sword-Main-350x350.webp" width=300>
+        <h4 style="line-height: 0px;">As low as $4.89 Bulk</h4>
+        <section style="position: relative; display: flex; flex-direction: row; gap: 10px; margin: auto; width: 23%; align-items: center; line-height: 0px;">
+          <button class="increments" on:click={() => incrementDown("count1")} style="cursor: pointer; padding: 2px 5px; border-radius: 20px; border: 1px solid black;">-</button>
+          <p>{count1}</p>
+          <button class="increments" on:click={() => incrementUp("count1")} style="cursor: pointer; padding: 3px 2px; border-radius: 20px; border: 1px solid black;">+</button>
+        </section> 
+        <h2>Light Up Pirate Rapier Expandable Sword</h2>
+        <h4 style="line-height: 0px;">$7.69</h4>
+        <button style="margin-bottom: 10px; cursor: pointer;">Add to Cart</button><br>
+        <button style="cursor: pointer;">Buy in Bulk</button>
+      </div>
+
+
+      <div class="menu-item">
+        <img src="src/assets/Light-Up-Expandable-Magic-Wizard-with-Prism-Ball-Main-350x350.webp" width=300>
+        <h4 style="line-height: 0px;">As low as $4.29 Bulk</h4>
+        <section style="position: relative; display: flex; flex-direction: row; gap: 10px; margin: auto; width: 23%; align-items: center; line-height: 0px;">
+          <button class="increments" on:click={() => incrementDown("count2")} style="cursor: pointer; padding: 2px 5px; border-radius: 20px; border: 1px solid black;">-</button>
+          <p>{count2}</p>
+          <button class="increments" on:click={() => incrementUp("count2")} style="cursor: pointer; padding: 3px 2px; border-radius: 20px; border: 1px solid black;">+</button>
+        </section> 
+        <h2>Light Up Pirate Rapier Expandable Sword</h2>
+        <h4 style="line-height: 0px;">$4.99</h4>
+        <button style="margin-bottom: 10px; cursor: pointer;">Add to Cart</button><br>
+        <button style="cursor: pointer;">Buy in Bulk</button>
+      </div>
+      <div class="menu-item">
+        <img src="src/assets/Galactic-LED-Expandable-Green-Light-Saber-Sword0-350x350.webp" width=300>
+        <h4 style="line-height: 0px;">As low as $3.79 Bulk</h4>
+        <section style="position: relative; display: flex; flex-direction: row; gap: 10px; margin: auto; width: 23%; align-items: center; line-height: 0px;">
+          <button class="increments" on:click={() => incrementDown("count3")} style="cursor: pointer; padding: 2px 5px; border-radius: 20px; border: 1px solid black;">-</button>
+          <p>{count3}</p>
+          <button class="increments" on:click={() => incrementUp("count3")} style="cursor: pointer; padding: 3px 2px; border-radius: 20px; border: 1px solid black;">+</button>
+        </section> 
+        <h2>Light Up Pirate Rapier Expandable Sword</h2>
+        <h4 style="line-height: 0px;">$4.49</h4>
+        <button style="margin-bottom: 10px; cursor: pointer;">Add to Cart</button><br>
+        <button style="cursor: pointer;">Buy in Bulk</button>
+      </div>
+      <div class="menu-item">
+        <img src="src/assets/LED-Shark-Light-Up-Saber-Sword-Main-350x350.webp" width=300>
+        <h4 style="line-height: 0px;">As low as $6.59 Bulk</h4>
+        <section style="position: relative; display: flex; flex-direction: row; gap: 10px; margin: auto; width: 23%; align-items: center; line-height: 0px;">
+          <button class="increments" on:click={() => incrementDown("count4")} style="cursor: pointer; padding: 2px 5px; border-radius: 20px; border: 1px solid black;">-</button>
+          <p>{count4}</p>
+          <button class="increments" on:click={() => incrementUp("count4")} style="cursor: pointer; padding: 3px 2px; border-radius: 20px; border: 1px solid black;">+</button>
+        </section> 
+        <h2>Light Up Pirate Rapier Expandable Sword</h2>
+        <h4 style="line-height: 0px;">$8.49</h4>
+        <button style="margin-bottom: 10px; cursor: pointer;">Add to Cart</button><br>
+        <button style="cursor: pointer;">Buy in Bulk</button>
+      </div>
     </div>
-    <div class="menu-item">
-      <img src="src/assets/Light-Up-Expandable-Magic-Wizard-with-Prism-Ball-Main-350x350.webp" width=300>
-      <h4 style="line-height: 0px;">As low as $4.29 Bulk</h4>
-      <section style="position: relative; display: flex; flex-direction: row; gap: 10px; margin: auto; width: 23%; align-items: center; line-height: 0px;">
-        <button class="increments" on:click={() => incrementDown("count2")} style="cursor: pointer; padding: 2px 5px; border-radius: 20px; border: 1px solid black;">-</button>
-        <p>{count2}</p>
-        <button class="increments" on:click={() => incrementUp("count2")} style="cursor: pointer; padding: 3px 2px; border-radius: 20px; border: 1px solid black;">+</button>
-      </section> 
-      <h2>Light Up Pirate Rapier Expandable Sword</h2>
-      <h4 style="line-height: 0px;">$4.99</h4>
-      <button style="margin-bottom: 10px; cursor: pointer;">Add to Cart</button><br>
-      <button style="cursor: pointer;">Buy in Bulk</button>
+  {:else}
+    <div class="products">
+      <div class="row-1">
+        <div class="menu-item">
+          <img src="src/assets/NASA-Custom-Flashing-Blinky-Light-Pin-Technical-Drawing-350x276.webp" width=300>
+          <h4 style="line-height: 0px;">As low as $4.89 Bulk</h4>
+          <section style="position: relative; display: flex; flex-direction: row; gap: 10px; margin: auto; width: 23%; align-items: center; line-height: 0px;">
+            <button class="increments" on:click={() => incrementDown("count1")} style="cursor: pointer; padding: 2px 5px; border-radius: 20px; border: 1px solid black;">-</button>
+            <p>{count1}</p>
+            <button class="increments" on:click={() => incrementUp("count1")} style="cursor: pointer; padding: 3px 2px; border-radius: 20px; border: 1px solid black;">+</button>
+          </section> 
+          <h2>Light Up Pirate Rapier Expandable Sword</h2>
+          <h4 style="line-height: 0px;">$7.69</h4>
+          <button style="margin-bottom: 10px; cursor: pointer;">Add to Cart</button><br>
+          <button style="cursor: pointer;">Buy in Bulk</button>
+        </div>
+        <div class="menu-item">
+          <img src="src/assets/black-light-feauxton-led-light-key-rings-350x350.webp" width=300>
+          <h4 style="line-height: 0px;">As low as $4.89 Bulk</h4>
+          <section style="position: relative; display: flex; flex-direction: row; gap: 10px; margin: auto; width: 23%; align-items: center; line-height: 0px;">
+            <button class="increments" on:click={() => incrementDown("count1")} style="cursor: pointer; padding: 2px 5px; border-radius: 20px; border: 1px solid black;">-</button>
+            <p>{count1}</p>
+            <button class="increments" on:click={() => incrementUp("count1")} style="cursor: pointer; padding: 3px 2px; border-radius: 20px; border: 1px solid black;">+</button>
+          </section> 
+          <h2>Light Up Pirate Rapier Expandable Sword</h2>
+          <h4 style="line-height: 0px;">$7.69</h4>
+          <button style="margin-bottom: 10px; cursor: pointer;">Add to Cart</button><br>
+          <button style="cursor: pointer;">Buy in Bulk</button>
+        </div>
+        <div class="menu-item">
+          <img src="src/assets/light-up-devil-horns-blue-350x350.webp" width=300>
+          <h4 style="line-height: 0px;">As low as $4.89 Bulk</h4>
+          <section style="position: relative; display: flex; flex-direction: row; gap: 10px; margin: auto; width: 23%; align-items: center; line-height: 0px;">
+            <button class="increments" on:click={() => incrementDown("count1")} style="cursor: pointer; padding: 2px 5px; border-radius: 20px; border: 1px solid black;">-</button>
+            <p>{count1}</p>
+            <button class="increments" on:click={() => incrementUp("count1")} style="cursor: pointer; padding: 3px 2px; border-radius: 20px; border: 1px solid black;">+</button>
+          </section> 
+          <h2>Light Up Pirate Rapier Expandable Sword</h2>
+          <h4 style="line-height: 0px;">$7.69</h4>
+          <button style="margin-bottom: 10px; cursor: pointer;">Add to Cart</button><br>
+          <button style="cursor: pointer;">Buy in Bulk</button>
+        </div>
+        <div class="menu-item">
+          <img src="src/assets/Slingshot-LIght-Up-Orange-350x350.webp" width=300>
+          <h4 style="line-height: 0px;">As low as $4.89 Bulk</h4>
+          <section style="position: relative; display: flex; flex-direction: row; gap: 10px; margin: auto; width: 23%; align-items: center; line-height: 0px;">
+            <button class="increments" on:click={() => incrementDown("count1")} style="cursor: pointer; padding: 2px 5px; border-radius: 20px; border: 1px solid black;">-</button>
+            <p>{count1}</p>
+            <button class="increments" on:click={() => incrementUp("count1")} style="cursor: pointer; padding: 3px 2px; border-radius: 20px; border: 1px solid black;">+</button>
+          </section> 
+          <h2>Light Up Pirate Rapier Expandable Sword</h2>
+          <h4 style="line-height: 0px;">$7.69</h4>
+          <button style="margin-bottom: 10px; cursor: pointer;">Add to Cart</button><br>
+          <button style="cursor: pointer;">Buy in Bulk</button>
+        </div>
+      </div>
+      <div class="divider"></div>
+      <div class="row-2">
+        <div class="menu-item">
+          <img src="src/assets/White-LED-Fiberoptic-Wand-350x350.webp" width=300>
+          <h4 style="line-height: 0px;">As low as $4.89 Bulk</h4>
+          <section style="position: relative; display: flex; flex-direction: row; gap: 10px; margin: auto; width: 23%; align-items: center; line-height: 0px;">
+            <button class="increments" on:click={() => incrementDown("count1")} style="cursor: pointer; padding: 2px 5px; border-radius: 20px; border: 1px solid black;">-</button>
+            <p>{count1}</p>
+            <button class="increments" on:click={() => incrementUp("count1")} style="cursor: pointer; padding: 3px 2px; border-radius: 20px; border: 1px solid black;">+</button>
+          </section> 
+          <h2>Light Up Pirate Rapier Expandable Sword</h2>
+          <h4 style="line-height: 0px;">$7.69</h4>
+          <button style="margin-bottom: 10px; cursor: pointer;">Add to Cart</button><br>
+          <button style="cursor: pointer;">Buy in Bulk</button>
+        </div>
+        <div class="menu-item">
+          <img src="src/assets/SoftBubbleRings-350x350.webp" width=300>
+          <h4 style="line-height: 0px;">As low as $4.89 Bulk</h4>
+          <section style="position: relative; display: flex; flex-direction: row; gap: 10px; margin: auto; width: 23%; align-items: center; line-height: 0px;">
+            <button class="increments" on:click={() => incrementDown("count1")} style="cursor: pointer; padding: 2px 5px; border-radius: 20px; border: 1px solid black;">-</button>
+            <p>{count1}</p>
+            <button class="increments" on:click={() => incrementUp("count1")} style="cursor: pointer; padding: 3px 2px; border-radius: 20px; border: 1px solid black;">+</button>
+          </section> 
+          <h2>Light Up Pirate Rapier Expandable Sword</h2>
+          <h4 style="line-height: 0px;">$7.69</h4>
+          <button style="margin-bottom: 10px; cursor: pointer;">Add to Cart</button><br>
+          <button style="cursor: pointer;">Buy in Bulk</button>
+        </div>
+        <div class="menu-item">
+          <img src="src/assets/3FreeBlinkeesofMagicMattsChoice-350x350.webp" width=300>
+          <h4 style="line-height: 0px;">As low as $4.89 Bulk</h4>
+          <section style="position: relative; display: flex; flex-direction: row; gap: 10px; margin: auto; width: 23%; align-items: center; line-height: 0px;">
+            <button class="increments" on:click={() => incrementDown("count1")} style="cursor: pointer; padding: 2px 5px; border-radius: 20px; border: 1px solid black;">-</button>
+            <p>{count1}</p>
+            <button class="increments" on:click={() => incrementUp("count1")} style="cursor: pointer; padding: 3px 2px; border-radius: 20px; border: 1px solid black;">+</button>
+          </section> 
+          <h2>Light Up Pirate Rapier Expandable Sword</h2>
+          <h4 style="line-height: 0px;">$7.69</h4>
+          <button style="margin-bottom: 10px; cursor: pointer;">Add to Cart</button><br>
+          <button style="cursor: pointer;">Buy in Bulk</button>
+        </div>
+        <div class="menu-item">
+          <img src="src/assets/WhiteCoasterLEDBase-350x350.webp" width=300>
+          <h4 style="line-height: 0px;">As low as $4.89 Bulk</h4>
+          <section style="position: relative; display: flex; flex-direction: row; gap: 10px; margin: auto; width: 23%; align-items: center; line-height: 0px;">
+            <button class="increments" on:click={() => incrementDown("count1")} style="cursor: pointer; padding: 2px 5px; border-radius: 20px; border: 1px solid black;">-</button>
+            <p>{count1}</p>
+            <button class="increments" on:click={() => incrementUp("count1")} style="cursor: pointer; padding: 3px 2px; border-radius: 20px; border: 1px solid black;">+</button>
+          </section> 
+          <h2>Light Up Pirate Rapier Expandable Sword</h2>
+          <h4 style="line-height: 0px;">$7.69</h4>
+          <button style="margin-bottom: 10px; cursor: pointer;">Add to Cart</button><br>
+          <button style="cursor: pointer;">Buy in Bulk</button>
+        </div>
+      </div>
     </div>
-    <div class="menu-item">
-      <img src="src/assets/Galactic-LED-Expandable-Green-Light-Saber-Sword0-350x350.webp" width=300>
-      <h4 style="line-height: 0px;">As low as $3.79 Bulk</h4>
-      <section style="position: relative; display: flex; flex-direction: row; gap: 10px; margin: auto; width: 23%; align-items: center; line-height: 0px;">
-        <button class="increments" on:click={() => incrementDown("count3")} style="cursor: pointer; padding: 2px 5px; border-radius: 20px; border: 1px solid black;">-</button>
-        <p>{count3}</p>
-        <button class="increments" on:click={() => incrementUp("count3")} style="cursor: pointer; padding: 3px 2px; border-radius: 20px; border: 1px solid black;">+</button>
-      </section> 
-      <h2>Light Up Pirate Rapier Expandable Sword</h2>
-      <h4 style="line-height: 0px;">$4.49</h4>
-      <button style="margin-bottom: 10px; cursor: pointer;">Add to Cart</button><br>
-      <button style="cursor: pointer;">Buy in Bulk</button>
-    </div>
-    <div class="menu-item">
-      <img src="src/assets/LED-Shark-Light-Up-Saber-Sword-Main-350x350.webp" width=300>
-      <h4 style="line-height: 0px;">As low as $6.59 Bulk</h4>
-      <section style="position: relative; display: flex; flex-direction: row; gap: 10px; margin: auto; width: 23%; align-items: center; line-height: 0px;">
-        <button class="increments" on:click={() => incrementDown("count4")} style="cursor: pointer; padding: 2px 5px; border-radius: 20px; border: 1px solid black;">-</button>
-        <p>{count4}</p>
-        <button class="increments" on:click={() => incrementUp("count4")} style="cursor: pointer; padding: 3px 2px; border-radius: 20px; border: 1px solid black;">+</button>
-      </section> 
-      <h2>Light Up Pirate Rapier Expandable Sword</h2>
-      <h4 style="line-height: 0px;">$8.49</h4>
-      <button style="margin-bottom: 10px; cursor: pointer;">Add to Cart</button><br>
-      <button style="cursor: pointer;">Buy in Bulk</button>
-    </div>
-  </div>
+  {/if}
 </main>
 <footer>
   <div class="upper-footer">
@@ -314,5 +439,31 @@
 
   .lower-footer {
     text-align: center;
+  }
+
+  .products {
+    position: relative;
+    display: flex;
+    flex-direction: column;
+    
+    align-items: center;
+  }
+
+  .row-1, .row-2 {
+    position: relative;
+    display: flex;
+    flex-direction: row;
+    justify-content: space-evenly;
+    align-items: center;
+    padding: 40px 0;
+  }
+
+  .divider {
+    position: relative;
+    
+    width: 90%;
+    height: 1px;
+
+    background-color: rgb(0,0,0,0.4)
   }
 </style>
